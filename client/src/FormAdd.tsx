cf29cdab40374c4412ef58642168972c3e154064
Vladislav <vladislav.ponomarenko9930@gmail.com>
2023-08-29 20:25:36 +0300

    eslint rules & form add

---
+++ client/src/FormAdd.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FormAdd(): JSX.Element {
+  return (
+    <div>
+      <form>
+        <input type='text' />
+        <input type='text' />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default FormAdd;
